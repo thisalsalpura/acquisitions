b5c4e2fd582ff47d3fcbd75e93c29c9828e1cb26
thisalsalpura <salpurathisal@gmail.com>
2025-12-22 11:55:09 +0530

    Setup Loggers and Middlewares

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import logger from '#config/logger.js';
+import helmet from "helmet";
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message.trim()) } }));
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+    logger.info('Request Received!');
+    res.status(200).send('Hello World!');
 });
 
 export default app;
